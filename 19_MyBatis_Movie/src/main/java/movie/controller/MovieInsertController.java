05a9cb4a51caf3ece05545da19ca5608bad723a3
youn1223 <youn1223@daum.net>
2024-06-27 09:46:44 +0900

    master 946

--- 19_MyBatis_Movie/src/main/java/movie/controller/MovieInsertController.java
+++ 19_MyBatis_Movie/src/main/java/movie/controller/MovieInsertController.java
@@ -37,9 +37,9 @@ public class MovieInsertController {
 			return mav;
 		}
 		
-		// master 추가함, kim 수정
 		// master 추가함
-		// kim 추가함
+		// master 추가함
+		
 		int cnt = -1; 
 		cnt = movieDao.insertMovie(movie);
 		
